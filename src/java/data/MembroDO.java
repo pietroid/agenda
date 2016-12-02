2c5dd9e41c8c01d6041c22b431b4c867e15c595c
Guilherme <Guilherme@Guilherme-PC>
2016-12-02 16:00:57 -0200

    ]

---
+++ src/java/data/MembroDO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class MembroDO {
+    private int MEMBERid;
+    private int GEid;
+    private int USUid;
+    private int ADM;
+}
