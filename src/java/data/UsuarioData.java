df72a0d48598c5f6a6dbb7a3345db2108ad99849
LUIS FELIPE <LUIS FELIPE@192.168.0.127>
2016-12-02 18:43:10 -0200

    Use Case 1007

--- src/java/data/UsuarioData.java
+++ src/java/data/UsuarioData.java
@@ -6,6 +6,28 @@ import utils.Transacao;
 
 public class UsuarioData {
 
+    public UsuarioDO buscar(String username, Transacao tr) throws Exception {
+        Connection con = tr.obterConexao();
+        String sql = "select * from agenda.Usuario where Username = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, username);
+        ResultSet rs = ps.executeQuery();
+        UsuarioDO usuario = null;
+        if (rs.next()) {
+            usuario = new UsuarioDO();
+            usuario.setUsername(rs.getString("Username"));
+            usuario.setSenha(rs.getString("Senha"));
+            usuario.setId(rs.getInt("USUid"));
+            usuario.setNome(rs.getString("USnome"));
+            usuario.setEmail(rs.getString("E-mail"));
+            usuario.setBio(rs.getString("USbio"));
+            usuario.setImagem(rs.getString("USimagem"));
+            usuario.setData(rs.getDate("USdata"));
+            usuario.setCurso(rs.getString("UScurso"));
+            usuario.setIngresso(rs.getDate("USingresso"));
+            usuario.setPasta(rs.getString("USpasta"));
+        }
   
-
-} // ContatoDatagg
+        return usuario;
+    }
+}
