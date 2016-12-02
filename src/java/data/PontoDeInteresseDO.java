0139dfa4c5fa799ec51462d3e9e46b86711e90fa
LUIS FELIPE <LUIS FELIPE@192.168.0.127>
2016-12-02 13:08:52 -0200

    PontoDeInteresseDO e PontoDeInteresseData

---
+++ src/java/data/PontoDeInteresseDO.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author LUIS FELIPE
+ */
+public class PontoDeInteresseDO {
+    
+    private int INSid;
+    private int EVEid;
+    private int USUid;
+    private String EVEdata;
+
+    public int getINSid() {
+        return INSid;
+    }
+
+    public void setINSid(int INSid) {
+        this.INSid = INSid;
+    }
+
+    public int getEVEid() {
+        return EVEid;
+    }
+
+    public void setEVEid(int EVEid) {
+        this.EVEid = EVEid;
+    }
+
+    public int getUSUid() {
+        return USUid;
+    }
+
+    public void setUSUid(int USUid) {
+        this.USUid = USUid;
+    }
+
+    public String getEVEdata() {
+        return EVEdata;
+    }
+
+    public void setEVEdata(String EVEdata) {
+        this.EVEdata = EVEdata;
+    }
+    
+}
