6f84925b22080d8b28b6169bcbeeeb6d541b7701
LUIS FELIPE <LUIS FELIPE@192.168.0.127>
2016-12-02 13:18:29 -0200

    PontoDeInteresseDO e PontoDeInteresseData

---
+++ src/java/data/PontoDeInteresseData.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.sql.*;
+import java.util.*;
+import utils.Transacao;
+
+/**
+ *
+ * @author LUIS FELIPE
+ */
+public class PontoDeInteresseData {
+     
+    
+}
